e7e550778ed0bf29270a2d597f8eb4756b04be5a
Kyle DuVal <51682638+kyleduval42@users.noreply.github.com>
2024-04-08 13:29:18 -0700

    Delete devel directory

--- devel/share/common-lisp/ros/slam_toolbox_msgs/srv/_package_ClearQueue.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package slam_toolbox_msgs-srv)
-(cl:export '(STATUS-VAL
-          STATUS
-))
--- devel/share/vesc_msgs/cmake/vesc_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(vesc_msgs_MSG_INCLUDE_DIRS "/home/f1tenth/f1Tenth_UCSB_2024/src/vesc/vesc_msgs/msg")
-set(vesc_msgs_MSG_DEPENDENCIES std_msgs)
