3d96bf86d3c3f14621c9237594f95e46f293463c
kyleduval42 <kyle.duval22@gmail.com>
2024-04-21 17:15:01 -0700

    infinity filter fix

--- src/f1tenth_simulator/node/disparity_extender.cpp
+++ src/f1tenth_simulator/node/disparity_extender.cpp
@@ -26,14 +26,14 @@ private:
     // Publish drive data
     ros::Publisher drive_pub;
 
-    // previous desired steering angle
-    const double Kp = 1.0;
-    const double Ki = 0.0001;
-    const double Kd = 1.0;
-    double prevError = 0.0;
-    double integral = 0.0;
-    double prev_angle = 0.0;
-    double targetDistance = 0.39;
+
+    double prev_angle = 0.0;          //previous desired steering angle
+    int inf_max = 5;                  //maximum consecutive ignored infinites
+    double first_angle_degree = 90;   //starting measured angle
+    double second_angle_degree = 315; //ending measured angle
+    
+
+
     ackermann_msgs::AckermannDriveStamped drive_st_msg;
     ackermann_msgs::AckermannDrive drive_msg;
 
@@ -69,41 +69,41 @@ public:
         laser_msg = scan_msg;
         std::vector<float> laser_ranges;
         laser_ranges = laser_msg.ranges;
-       // size_t range_size = laser_ranges.size();
+        size_t range_size = laser_ranges.size();
 
         
-	double angle_increment = laser_msg.angle_increment; //tells the Lidar the angle to scan
-	double first_angle_degree = 45;
-	double first_angle_radian = first_angle_degree * M_PI / 180.0;
-	double second_angle_degree = 315;
-	double second_angle_radian = second_angle_degree * M_PI / 180.0;
+	    double angle_increment = laser_msg.angle_increment; //tells the Lidar the angle to scan
+	    double first_angle_radian = first_angle_degree * M_PI / 180.0;
+	    double second_angle_radian = second_angle_degree * M_PI / 180.0;
+   
+        size_t first_index = static_cast<size_t>(first_angle_radian / angle_increment); //finds the starting and ending index for laser_ranges
+	    size_t second_index = static_cast<size_t>(second_angle_radian / angle_increment);
 
-	//double theta = second_angle_radian - first_angle_radian;       
-    size_t first_index = static_cast<size_t>(first_angle_radian / angle_increment); //finds the starting and ending index for laser_ranges
-	size_t second_index = static_cast<size_t>(second_angle_radian / angle_increment);
+        double largest_disp = -1;   //finds the index and value of the largest range disparity
+        double wall_dist = -1;      //finds the distance to the inside turning wall
+        int largest_disp_idx = -1;
+        bool left;                 //keeps track of turning direction
+
+    
+        for(size_t i = first_index; i <= second_index - inf_max;i++){ //rewrite infinites as the previous non infinite number if less than inf_max consecutive infinties
+            if (laser_ranges[i] == INFINITY){
+                 int count = 1;
+                while (count < inf_max && laser_ranges[i + count] == INFINITY){ //while counting to inf_max until it hits a non infinte number
+                    count++;
+                }
+                if (count <= inf_max){                                 //if there were under inf_max consecutive infinites
+                    for (int j = 0; j < count; j++){
+                        laser_ranges[i + j] = laser_ranges[i];         //write all of the infinites as the first indexed value
+                    }
+                }
 
 
-    double largest_disp = -1;   //finds the index and value of the largest range disparity
-    double wall_dist = -1;      //finds the distance to the inside turning wall
-    int largest_disp_idx = -1;
-    bool left;                 //keeps track of turning direction
-
-    /*
-    for(size_t i = first_index; i <= second_index - 1;i++){ //rewrite infinites as the previous non infinite number
-        if (laser_ranges[i] == INFINITY){
-            size_t a = i;
-            while (laser_ranges[i] > 30 && a > 0)
-            {
-                a = a -1;
-                laser_ranges[i] = laser_ranges[a];
             }
-            
         }
-    }
-    */
 
+    
     int steering_angle_center_idx = (second_index - first_index); 
-
+    
     for (size_t i = first_index; i <= second_index - 1; i++){ 
 
         double curr = laser_ranges[i];
@@ -114,7 +114,7 @@ public:
             largest_disp_idx = i;               //writes the new largest index
 
             if (i <= steering_angle_center_idx){
-                left = true;              //its a LEFT turn! THESE WILL CHANGE PER SIM AND IRL
+                left = true;              //its a LEFT turn! THESE WILL CHANGE PER SIM AND IRL maybe
                 wall_dist = curr;         
             }  
             if (i > steering_angle_center_idx){
@@ -150,25 +150,18 @@ public:
   	drive_st_msg.drive = drive_msg;
   	drive_pub.publish(drive_st_msg);
 
-    //std::cout << boolalpha;
-    //std::cout << "LEFT?: "<<left<< " Steering angle:" << steering_angle_disp << "correction: " << correction <<std::endl; //print statements for debugging REMOVE LATER
+    //debugging print statements:
+
+    std::cout << boolalpha;
+    //std::cout << "LEFT?: "<<left<< " Steering angle:" << steering_angle_disp << "correction: " << correction <<std::endl; 
+    //std::cout << "Starting ind: " << first_index << "Ending ind: " << second_index << std::endl;
     
     
-    
-    for (size_t j = first_index; j < second_index; j++){
+    for (size_t j = first_index; j < second_index; j++){ //prints all measured lidar values
         std::cout << laser_ranges[j] << ", ";
     }
         std::cout << endl << "---SEPERATOR---" << endl;
-    
 
-	//ROS_INFO("[ROBOT] Dist: %f", distance);
-	//ROS_INFO("[ROBOT] 90: %f", a);
-	//ROS_INFO("[ROBOT] 120: %f", b);
-        //ROS_INFO("[ROBOT] Porp: %f", error*Kp);
-        //ROS_INFO("[ROBOT] Integ: %f", integral*Ki);
-        //ROS_INFO("[ROBOT] Deriv: %f", derivative*Kd);
-        //ROS_INFO("[ROBOT] E: %f", error);
-        //ROS_INFO("[ROBOT] angle: %f", steering_angle);
     }
 };
 // end of class definition
